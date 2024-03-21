1edf6f1a17544dacea297a2b33bff73c7afd7e8f
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-21 14:54:11 +0700

    create UsersController with index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "posts#index"
+  root 'posts#index'
   resources :users
   resources :posts
   devise_for :users, path: 'accounts'
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
