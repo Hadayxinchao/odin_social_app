7af65d7c6b9c91a5c16032ac83d3039949166759
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-21 18:42:18 +0700

    Create Comment model, migrate and configure its associations

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+  belongs_to :post
+  belongs_to :author, class: 'User', foreign_key: 'user_id'
+  has_many :likes, as: :likeable, dependent: :destroy
+  has_many :notifications, as: :notificationable, dependent: :destroy
+  validates :content, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_21_102722) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_21_113928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "friendships", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -73,6 +83,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_21_102722) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "friendships", "users"
   add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "likes", "users"
