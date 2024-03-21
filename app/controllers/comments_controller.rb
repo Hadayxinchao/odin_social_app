3f1500c77a720d864e21ef5958ddc83ea60f5956
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-21 18:56:04 +0700

    Create form for comments on posts and define comments#create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,20 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      flash[:success] = 'Comment created'
+    else
+      flash[:error] = 'Invalid Comment'
+    end
+    redirect_back_or_to root_path
   end
 
   def destroy
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :post_id)
   end
 end
