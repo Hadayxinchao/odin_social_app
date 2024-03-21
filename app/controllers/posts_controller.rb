be7236588dc4110149da73cd73e531787031beb4
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-21 17:08:24 +0700

    Allow creating Posts with viewing own' and friends's posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,31 @@
 class PostsController < ApplicationController
   def index
+    friends_ids = Friendship.where(user_id: current_user.id, status: 2).pluck(:friend_id)
+    @posts = Post.where(user_id: current_user.id)
+                 .or(Post.where(user_id: friends_ids))
+                 .order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_back_or_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :user_id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   validates :content, presence: true
 end
