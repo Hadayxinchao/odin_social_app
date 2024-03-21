08d5173d2517e320f559877ae3e224eca212949b
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-21 18:47:17 +0700

    Correct option typo on belongs_to declaration

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  belongs_to :author, class: 'User', foreign_key: 'user_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :likes, as: :likeable, dependent: :destroy
   has_many :notifications, as: :notificationable, dependent: :destroy
   validates :content, presence: true
