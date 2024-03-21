8ce661924698940e2ccdd2b64bd1ac505af76426
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-21 16:07:17 +0700

    Allow accepting or rejecting a friendship request

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,18 +6,30 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    Friendship.create(user_id: params[:user_id], friend_id: params[:friend_id], status: 1)
-    Friendship.create(user_id: params[:friend_id], friend_id: params[:user_id], status: 2)
+    Friendship.create(user_id: params[:user_id],
+                      friend_id: params[:friend_id],
+                      status: 1)
+    Friendship.create(user_id: params[:friend_id],
+                      friend_id: params[:user_id],
+                      status: 1)
     @friend = User.find(params[:friend_id])
     redirect_to @friend
   end
 
   def update
+    @friendship = Friendship.find(params[:id])
+    @friendship2 = Friendship.find_by(user_id: @friendship.friend_id,
+                                      friend_id: @friendship.user_id)
+    @friend = @friendship.friend
+    @friendship.update(status: 2)
+    @friendship2.update(status: 2)
+    redirect_to @friend
   end
 
   def destroy
     @friendship = Friendship.find(params[:id])
-    @friendship2 = Friendship.find_by(user_id: @friendship.friend_id, friend_id: @friendship.user_id)
+    @friendship2 = Friendship.find_by(user_id: @friendship.friend_id,
+                                      friend_id: @friendship.user_id)
     @friend = @friendship.friend
     @friendship.destroy
     @friendship2.destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @friendship = Friendship.find_by(user_id: current_user.id, friend_id: @user.id)
+    @friendship1 = Friendship.find_by user_id: current_user.id, friend_id: @user.id
+    @friendship2 = Friendship.find_by user_id: @user.id, friend_id: current_user.id
+
+    # Checks if friendship request exists
+    if @friendship1
+      # Tells the role of current_user on the Friendship request
+      @role = if @friendship2.id > @friendship1.id
+                'requester'
+              else
+                'requested'
+              end
+    end
   end
 
   def index
