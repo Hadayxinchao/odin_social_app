9425d377d24ad20049bb379f4df259f5a0f04947
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-21 14:45:39 +0700

    Add first_name, last_name and photos to users table

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :trackable, :timeoutable
+  has_many :posts, inverse_of: 'author', dependent: :destroy
+  validates :email, presence: true
+end
