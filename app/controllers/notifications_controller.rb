4c54dbf9061b141ed350b2c0c919e67d30bd6c94
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-21 16:40:01 +0700

    Allow delete notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,5 +6,8 @@ class NotificationsController < ApplicationController
   end
 
   def destroy
+    @notification = Notification.find(params[:id])
+    @notification.destroy
+    redirect_back_or_to root_path
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :notificationable, polymorphic: true
-  validates :text, presence: true
+  validates :content, presence: true
 end
