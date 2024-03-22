05e6a1d85ff3d24bb1e6f93823b58a3ddf98f870
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-22 08:36:29 +0700

    Allow liking and unliking comments and notifies author of comment

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   def create
     like = Like.create(like_params)
 
-    # Creates a notification for the user who created the post
+    # Creates a notification unless like author is the post/comment author
     unless like.user_id == like.likeable.user_id
       Notification.create(notificationable_id: like.id,
                           notificationable_type: 'Like',
                           user_id: like.likeable.author.id,
-                          content: "#{like.user.email} liked your post")
+                          content: notification_text(like))
     end
     redirect_back_or_to root_path
   end
@@ -18,6 +18,14 @@ class LikesController < ApplicationController
   end
 
   private
+
+  def notification_text(like)
+    if like.likeable_type == 'Comment'
+      "#{like.user.email} liked your comment"
+    elsif like.likeable_type == 'Post'
+      "#{like.user.email} liked your post"
+    end
+  end
 
   def like_params
     params.require(:like).permit(:user_id, :likeable_id, :likeable_type)
