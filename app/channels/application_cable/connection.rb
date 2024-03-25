756fb2bca944ca3ebed4d6f0ad8aae5091261f94
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-25 17:04:05 +0700

    Try websocket reconnections until connection is successfull

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -8,6 +8,18 @@ module ApplicationCable
 
     private
 
+    def handle_open
+      @protocol = websocket.protocol
+      connect if respond_to?(:connect)
+      subscribe_to_internal_channel
+      send_welcome_message
+
+      message_buffer.process!
+      server.add_connection(self)
+    rescue ActionCable::Connection::Authorization::UnauthorizedError
+      close(reason: ActionCable::INTERNAL[:disconnect_reasons][:unauthorized], reconnect: true) if websocket.alive?
+    end
+
     def find_verified_user
       if verified_user = env['warden'].user
         verified_user
