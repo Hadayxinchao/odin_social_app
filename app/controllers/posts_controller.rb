c508d16b0a839c9cb257f570375706c40d9f060e
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-25 16:55:06 +0700

    Filter posts#show views for only own and friend's posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :check_authorization, only: %i[destroy]
+  before_action :check_authorization_for_destroy, only: %i[destroy]
+  before_action :check_authorization_for_show, only: %i[show]
   def index
     friends_ids = Friendship.where(user_id: current_user.id, status: 2).pluck(:friend_id)
     @post = Post.new
@@ -47,11 +48,18 @@ class PostsController < ApplicationController
     params.require(:post).permit(:content)
   end
 
-  def check_authorization
+  def check_authorization_for_destroy
     post = Post.find(params[:id])
     return if post.author == current_user
 
     flash[:error] = 'You are not authorized to delete this post'
+    redirect_back_or_to root_path, status: :forbidden
+  end
+
+  def check_authorization_for_show
+    post = Post.find(params[:id])
+    return if post.author == current_user || post.author.friends.exists?(id: current_user.id)
+
     redirect_back_or_to root_path, status: :forbidden
   end
 end
