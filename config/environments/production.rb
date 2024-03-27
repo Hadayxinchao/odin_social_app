ca2314eda3204ad0878de6090f357c2c730c591b
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-27 09:49:20 +0700

    Allow requests from custom host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << "fathomless-brushlands-47894-f147b656a22f.herokuapp.com"
 end
