62784275a5776b7619ae8bd230c7837764ed4680
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-27 18:00:35 +0700

    Fix bug: Update app_password for deliver emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,12 +40,21 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  
-  config.action_mailer.delivery_method = :letter_opener
-  
+
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            587,
+    domain:          'example.com',
+    user_name:       Rails.application.credentials.dig(:gmail, :user_name),
+    password:        Rails.application.credentials.dig(:gmail, :password),
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
