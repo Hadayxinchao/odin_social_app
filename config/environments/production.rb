99c16a2307f8e0ac3b16e245a1b7ed2a206168b1
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-27 13:38:11 +0700

    Define allowed request origins for websocket connections in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ "https://stormy-tundra-18959-4ac50ee587d1.herokuapp.com/", /http:\/\/stormy-tundra-18959-4ac50ee587d1.*/ ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
